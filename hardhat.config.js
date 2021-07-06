5a2e0151bfba7c1cd65f2a19bf22731194ce56b7
Alexey <peretsmobil@gmail.com>
2021-07-07 00:11:30 +0300

    OVM

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require('@nomiclabs/hardhat-waffle')
+require('@eth-optimism/hardhat-ovm')
 require('dotenv').config()
 
 const config = {
@@ -11,6 +12,9 @@ const config = {
       },
     },
   },
+  ovm: {
+    solcVersion: '0.7.6', // Your version goes here.
+  },
   networks: {
     // goerli: {
     //   url: process.env.ETH_RPC,
@@ -20,6 +24,19 @@ const config = {
     //         mnemonic: 'test test test test test test test test test test test junk',
     //       },
     // },
+    optimism: {
+      url: process.env.ETH_RPC,
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : {
+            mnemonic: 'test test test test test test test test test test test junk',
+          },
+      // This sets the gas price to 0 for all transactions on L2. We do this
+      // because account balances are not automatically initiated with an ETH
+      // balance (yet, sorry!).
+      gasPrice: 15000000,
+      ovm: true, // This sets the network as using the ovm and ensure contract will be compiled against that.
+    },
   },
   mocha: {
     timeout: 600000000,
